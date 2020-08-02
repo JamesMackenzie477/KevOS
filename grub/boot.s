51833b4caa2ec3646377eaa7b48cf8f6ab7ba69e
JamesMackenzie477 <JamesMackenzie477@gmail.com>
2020-08-02 20:20:11 +0100

    Started implementation of paging.

--- grub/boot.s
+++ grub/boot.s
@@ -18,8 +18,15 @@ stack_top:
  
  // Sets up the stack and bridges to the C code.
 .section .text
+
 .global _start
+.global _set_page_dir
+.global _enable_paging
+
 .type _start, @function
+.type _set_page_dir, @function
+.type _enable_paging, @function
+
 _start:
 	mov $stack_top, %esp
 	call kernel_main
@@ -27,3 +34,22 @@ _start:
 loop:
 	hlt
 	jmp loop
+
+_enable_paging:
+	push %ebp
+	mov %esp, %ebp
+	mov %cr0, %eax
+	or $0x80000000, %eax
+	mov %eax, %cr0
+	mov %ebp, %esp
+	pop %ebp
+	ret
+
+_set_page_dir:
+	push %ebp
+	mov %esp, %ebp
+	mov 8(%esp), %eax
+	mov %eax, %cr3
+	mov %ebp, %esp
+	pop %ebp
+	ret
