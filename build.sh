d4e52d540aebaa2fa04cf82e21ea7083314a0d77
JamesMackenzie477 <JamesMackenzie477@gmail.com>
2021-01-17 12:28:10 +0000

    Changed project name to KevOS.

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ tar cvf ../build/disk.tar disk/
 cd ../build
 $HOME/opt/cross/bin/i686-elf-as ../source/asm/*.s -o asm.o
 $HOME/opt/cross/bin/i686-elf-gcc -c ../source/*.c -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-$HOME/opt/cross/bin/i686-elf-gcc -T ../source/linker.ld -o kevin.bin -ffreestanding -O2 -nostdlib *.o -lgcc
-grub-file --is-x86-multiboot kevin.bin
-cp kevin.bin iso/boot/kevin.bin
+$HOME/opt/cross/bin/i686-elf-gcc -T ../source/linker.ld -o KevOS.bin -ffreestanding -O2 -nostdlib *.o -lgcc
+grub-file --is-x86-multiboot KevOS.bin
+cp KevOS.bin iso/boot/KevOS.bin
 cp ../source/grub/grub.cfg iso/boot/grub/grub.cfg
-grub-mkrescue -o kevin.iso iso
+grub-mkrescue -o KevOS.iso iso
 rm *.o *.bin *.tar
 rm -r iso
-qemu-system-i386.exe -cdrom kevin.iso
+qemu-system-i386.exe -cdrom KevOS.iso
 cd ..
