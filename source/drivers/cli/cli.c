97188ee59fb2df5a6e28366077f5bf4096bc66cd
JamesMackenzie477 <jamesmackenzie477@gmail.com>
2022-09-02 18:11:02 +0100

    Updated source files to reflect finished OS.

--- source/drivers/cli/cli.c
+++ source/drivers/cli/cli.c
@@ -5,24 +5,6 @@
  */
 int main(void)
 {
-	char buff[10] = { 0 };
-	// Waits for user input.
-	while (1)
-	{
-		// Prints the console string to the screen.
-		printf("KevOS> ");
-		printf("ls\n");
-		printf("cli.bin\n");
-		printf("KevOS> fread(&buff, 1, 1)\n");
-
-		size_t r = __fread(&buff, 1, 1);
-
-		printf("r = 0x%x\n", r);
-		printf("KevOS> ");
-		// Gets the user input.
-		// Parses the user input.
-		break;
-	}
-	// Success.
-	return 1;
+	// Hello world!
+	printf("Hello world!");
 }
--- source/drivers/kevin.s
+++ source/drivers/kevin.s
@@ -1,51 +1,24 @@
 .section .text
 
 // Macro to create a new system call.
-.macro sysdef name id=0 // argc=0
-
+.macro sysdef name id=0
 	.global \name
 	.type \name, @function
-
 	\name:
-		// Understanding the Linux Kernel (Page 409 - Parameter Passing).
-		// Registers to be used if arguments are 3 or less. Else we out the rest in a memory region pointed to by the final parameter.
-
-		// .if (\argc == 1)
-
-			// mov 4(%esp), %ebx
-
-		// .elseif (\argc == 2)
-
-			// mov 4(%esp), %ebx
-			// mov 8(%esp), %esi
-
-		// .elseif (\argc == 3)
-
-			mov 4(%esp), %ebx
-			mov 8(%esp), %esi
-			mov 12(%esp), %edi
-
-		// .elseif (\argc > 3)
-
-			// mov 4(%esp), %ebx
-			// mov 8(%esp), %esi
-			// mov %esp+12, %edi
-
-		// .endif
-
+		// Arguments.
+		mov 4(%esp), %ebx
+		mov 8(%esp), %esi
+		mov 12(%esp), %edi
 		// The system call id to invoke.
 		mov $\id, %eax
-
 		// Tells the kernel the return address.
 		pop %edx
 		// Passes the user stack to the kernel.
 		mov %esp, %ecx
-
 		// Trap to the kernel.
 		sysenter	
-
 .endm
 
 // Creates a new syscall called printf that triggers the zeroth system function.
-sysdef __printf 0x0 // 2
-sysdef __fread 0x2 // 3
+sysdef __printf 0x0
+sysdef __fread 0x2
